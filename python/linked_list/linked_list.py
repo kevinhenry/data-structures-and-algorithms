52151a8f7be9953180c3af770c26086ea5b5b4a0
Kevin Henry <kevinhenry@mac.com>
2021-06-21 00:15:32 -0700

    Three tests passing

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,75 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
 class LinkedList:
-    """
-    Put docstring here
-    """
+    def __init__(self, head=None):
+        self.head = head
 
-    def __init__(self):
-        # initialization here
-        pass
+    def insert(self, value):
+        node = Node(value)
 
-    def some_method(self):
-        # method body here
-        pass
+        if self.head is not None:
+            node.next = self.head  # pear -> orange
+            self.head = node  # HEAD: pear -> orange
+
+    def includes(self, value):
+        current = self.head
+
+        while current is not None:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+
+    #
+
+    #     node = node.next:
+    # return False
+
+    # pph
+
+    # computational thinking. takes a massive problem and seperates it down into small little challenges = litte wins.
+
+    # get into the habbit of is doing the write out.
+
+    # ll2.head.value = pear
+    # ll2.head.next = orange
+    # ll2.head.next.next = apple
+
+    # ll2 pears -> orange -> apple -> None
+
+    # current = 112.head
+    # while current.value  is not None:
+    #     # do something
+    #     current = current.next
+
+    # if __name__ == "__main__":
+    #     ll2 = LinkedList()
+    #     ll2.insert('apples')
+    #     ll2.insert('orange')
+    #     ll2.insert('pears')
+
+    # function link list head -> apples? / mope -> apples yet? / nope -> apples yet? / true
+    # if linked list's value becomes none, That is the end of the linked list.
+
+    # while loop - if value is in the linked list, return items, if not ten return None
+
+    # ll = LinkedList()
+    # node1 = Node("apples")
+    # node2 = Node("orange")
+    # node3 = Node("pear")
+    # ll.head == node1
+    # node1.next == node2
+    # node2.next == node3
+
+    # node1.next == None
+
+    # llhead = apple -> 'orange' -> 'pear' -> None
+
+    # ll1 = LinkedList(Node("apples", Node("orange", Node("pear"))))
+
+    # llhead = apples -> 'orange' -> 'pear' -> None
+    # ll1.next = None
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,28 @@
-from linked_list.linked_list import LinkedList
+from linked_list.linked_list import LinkedList, Node
 
 
-def test_import():
-    assert LinkedList
+def test_node_instance():
+    node = Node("apples", None)
+    actual = node.value
+    expected = "apples"
+    assert actual == expected
+    assert node.next == None
+
+
+def test_two_nodes():
+    node2 = Node("orange", None)
+    node1 = Node("apples", node2)
+    actual = node1.next.value
+    expected = "orange"
+    assert actual == expected
+
+
+# def test_detect():
+#     actual = LinkedList(includes)
+#     expected = True
+#     assert actual == expected
+
+
+def test_empty_ll():
+    ll = LinkedList()
+    assert ll
